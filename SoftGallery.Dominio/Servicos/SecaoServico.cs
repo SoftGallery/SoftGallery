9074d08b52d619f26697c84fd98837aab5d04812
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-03-21 22:26:46 -0300

    Inicio services Produto e Seção.

---
+++ SoftGallery.Dominio/Servicos/SecaoServico.cs
@@ -0,0 +1,24 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftGallery.Dominio.Servicos
+{
+    public class SecaoServico
+    {
+        private readonly ISessionFactory session;
+
+        public SecaoServico(ISessionFactory session)
+        {
+            this.session = session;
+        }
+
+        public List<Entidades.Secao> Listar()
+        {
+            session.Query<S
+        }
+    }
+}
