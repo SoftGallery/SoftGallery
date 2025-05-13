380ebaa5cf4aafdcf145dda7cec4f009aa5028bf
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-05-13 17:41:02 -0300

    Committ 03.3

--- SoftGallery.Web/src/components/CardProduto.vue
+++ SoftGallery.Web/src/components/CardProduto.vue
@@ -1,33 +1,46 @@
 <template>
-      <el-card style="border-radius: 15px;" :body-style="{ padding: '1px' }">
-        <div style="display: flex; justify-content: center; align-items: center; position: relative;">
-          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-            class="image" style="display: block; margin: 0 auto; width: 100%;">
-          <HeartButton style="position: absolute; top: 10px; right: 10px;"></HeartButton>
+  <el-card style="border-radius: 15px;" :body-style="{ padding: '1px' }">
+    <div style="display: flex; justify-content: center; align-items: center; position: relative;">
+      <div style="position: absolute;top: 10px;left: 10px;background-color: red;color: white;padding: 4px 8px;border-radius: 10px;font-size: 12px;font-weight: bold;
+">
+        { Desconto }
+      </div>
 
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+        class="image" style="display: block; margin: 0 auto; width: 100%;">
+      <HeartButton style="position: absolute; top: 10px; right: 10px;"></HeartButton>
+
+    </div>
+    <div style="padding: 14px;">
+      <el-card style="border: none; box-shadow: none;" class="box-card">
+        <div slot="header" class="clearfix">
+          <h1>
+            <span>Smartphone XYZ Pro</span>
+          </h1>
         </div>
-        <div style="padding: 14px;">
-          <el-card style="border: none; box-shadow: none;" class="box-card">
-            <div slot="header" class="clearfix">
-              <h1>
-                <span>Smartphone XYZ Pro</span>
-              </h1>
+        <div class="block" style="margin-top: 14px;">
+          <h4>
+            <span class="demonstration">4.5</span>
+          </h4>
+          <el-rate v-model="value2" :colors="colors"></el-rate>
+        </div>
+        <div style="margin-top: 10px;"  >
+          <CartButton style="align-items: end;"></CartButton>
+          <div class="preco-container" style="align-items: start;">
+            <div class="preco-original">
+              R$ {{ 200}}
             </div>
-            <div class="block" style="margin-top: 14px;">
-              <h4>
-                <span class="demonstration">4.5</span>
-              </h4>
-              <el-rate v-model="value2" :colors="colors"></el-rate>
+            <div class="preco-atual">
+              R$ {100}
             </div>
-            <div style="margin-top: 16px; text-align: right;">
-              <CartButton></CartButton>
-            </div>
-          </el-card>
-          <div class="bottom clearfix" style="margin-top: 10px;">
-            <time class="time">{{ currentDate }}</time>
           </div>
         </div>
       </el-card>
+      <div class="bottom clearfix" style="margin-top: 10px;">
+        <time class="time">{{ currentDate }}</time>
+      </div>
+    </div>
+  </el-card>
 </template>
 <script>
 import CartButton from './CartButton.vue';
@@ -43,6 +56,10 @@ export default {
     nome_produto: {
       type: String,
       required: true
+    },
+    porcentagem_desconto: {
+      type: Number,
+      required: false
     }
   },
   components: {
@@ -54,3 +71,25 @@ export default {
 }
 
 </script>
+
+<style>
+.preco-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 10px;
+}
+
+.preco-atual {
+  font-size: 18px;
+  font-weight: bold;
+  color: #67c23a; /* verde */
+}
+
+.preco-original {
+  font-size: 14px;
+  text-decoration: line-through;
+  color: #aaa;
+}
+
+</style>
