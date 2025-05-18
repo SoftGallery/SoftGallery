f324b036092182ded85d1d7f8a7f07cf5d1d87b9
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-05-18 10:55:04 -0300

    add atualização back

---
+++ SoftGallery/SoftGallery.Dominio/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftGallery.Dominio.Models
+{
+    public class Produto
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+        public Produto(string id, string nome, string descricao, decimal preco)
+        {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+        }
+
+        public Produto() { }
+    }
+}
