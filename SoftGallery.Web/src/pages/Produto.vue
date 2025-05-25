55181b10938ad61630ff33305d5465d6f5116557
Roddy192 <rodrigo.bgarcia@hotmail.com>
2025-05-25 18:39:31 -0300

    Commit Rodrigo

---
+++ SoftGallery.Web/src/pages/Produto.vue
@@ -0,0 +1,29 @@
+
+<script lang="ts" setup>
+//template e script - o Router-view que está na "app.vue" exibe dinamicamente os conteúdos que estão  determinados nas rotas. Na Rota 'Home' estão importados os ocmponentes de 'section, cards , etc' - vou criar a página Produto que vai ser uma Rota onde vou importar os componentes e exebir um HTML. É só seguir o exemplo da Página Home.vue.
+
+import CardProduto from '../components/CardProduto.vue';
+import Section from '../components/Section.vue';
+import Footer from '../components/Footer.vue';
+</script>
+
+<template>
+    <div style="display: flex; justify-content: center; align-items: center; height: 500px; width:  500px;">
+        <div style="position: absolute; top: 8px; left: 8px; background-color: red; color: white; padding: 2px 6px; border-radius: 8px; font-size: 11px; font-weight: bold;">
+        </div>
+        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+         style="width: 100%; height: 100%; object-fit: cover; align-items: center;">
+        <HeartButton style="position: absolute; top: 8px; right: 8px;"></HeartButton>
+        <h2>Nome do Produto</h2>
+        <div style="background-color: lightgray;">
+            <h3>Descrição:</h3>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+        </div>
+        <div style="display: flex;">
+            <div>Preço</div>
+            <div>Quantidade</div>
+        </div>
+    </div>
+
+<Footer></Footer>
+</template>
--- SoftGallery.Web/src/routes.ts
+++ SoftGallery.Web/src/routes.ts
@@ -2,12 +2,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from "./pages/Home.vue"
 import LoginAdm from "./pages/LoginAdm.vue"
+import Produto from "./pages/Produto.vue"
 
 
 const routes = [
   { path: '/', component: Home },
   { path: '/login-adm', component: LoginAdm },
-  { path: '/contato', component: LoginAdm }
+  { path: '/contato', component: LoginAdm },
+  { path: '/produto', component: Produto }
 ]
 
 const router = createRouter({
