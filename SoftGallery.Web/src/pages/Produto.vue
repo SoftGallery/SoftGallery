e3b249ef9932f0b413a0929b7dee447d69e399a8
Roddy192 <149844471+Roddy192@users.noreply.github.com>
2025-06-08 12:49:30 -0300

    commit pagina de produto - Rodrigo
    Ainda precisa ajustar algumas cosas

--- SoftGallery.Web/src/pages/Produto.vue
+++ SoftGallery.Web/src/pages/Produto.vue
@@ -5,23 +5,39 @@
 import CardProduto from '../components/CardProduto.vue';
 import Section from '../components/Section.vue';
 import Footer from '../components/Footer.vue';
+
 </script>
 
 <template>
-    <div style="display: flex; justify-content: center; align-items: center; height: 500px; width:  500px;">
-        <div style="position: absolute; top: 8px; left: 8px; background-color: red; color: white; padding: 2px 6px; border-radius: 8px; font-size: 11px; font-weight: bold;">
-        </div>
-        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-         style="width: 100%; height: 100%; object-fit: cover; align-items: center;">
-        <HeartButton style="position: absolute; top: 8px; right: 8px;"></HeartButton>
-        <h2>Nome do Produto</h2>
-        <div style="background-color: lightgray;">
-            <h3>Descrição:</h3>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
-        </div>
-        <div style="display: flex;">
-            <div>Preço</div>
-            <div>Quantidade</div>
+    <div style="height: 1000px; display: flex; justify-content: center; align-items: center;">
+        <div style="display: flex; align-items: center; height: 70%; width:70%;">
+            <div style="display: flex; flex-direction: column; height: 100%; width: 10%;">
+                <img src="https://placehold.co/100x100" alt="" style="max-width: 100%; max-height: 100%; overflow:hidden; height: 100%; border: 2px solid white">
+                <img src="https://placehold.co/100x100" alt="" style="max-width: 100%; max-height: 100%; overflow:hidden; height: 100%; border: 2px solid white">
+                <img src="https://placehold.co/100x100" alt="" style="max-width: 100%; max-height: 100%; overflow:hidden; height: 100%; border: 2px solid white">
+                <img src="https://placehold.co/100x100" alt="" style="max-width: 100%; max-height: 100%; overflow:hidden; height: 100%; border: 2px solid white">
+                <img src="https://placehold.co/100x100" alt="" style="max-width: 100%; max-height: 100%; overflow:hidden; height: 100%; border: 2px solid white">
+                <img src="https://placehold.co/100x100" alt="" style="max-width: 100%; max-height: 100%; overflow:hidden; height: 100%; border: 2px solid white">
+                
+            </div>
+            <div style="height: 100%; width: 40%; margin-left: 20px;">
+                <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                 style="width: 100%; height: 100%; object-fit: cover; align-items: center;">
+            </div>
+            <div style="display: flex; flex-direction: column; justify-content: flex-start; gap: 50px; height: 100%; width: 50%; margin-left: 20px;">
+                <h2 style="color: black; font-weight:800; font-size: 35px;">Nome do Produto</h2>
+                <div style="display: flex; color: black; font-size: 35px;">
+                    <div>$1500</div>
+                    <span style="text-decoration-line: line-through; font-size: 30px; margin-left: 5px; color: darkgray;">Desconto</span>
+                </div>
+                <div style="background-color: lightgray;">
+                    <h3>Descrição:</h3>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                </div>
+                <div style="display: flex; justify-content: center; align-items: center; border-color: black; border-style: solid;">
+                    <button style="background-color: rgb(24, 24, 27); color: white; padding: 0px 30px; border-radius: 3px; height: 60px;">Add to cart</button>
+                </div>
+            </div>
         </div>
     </div>
 
